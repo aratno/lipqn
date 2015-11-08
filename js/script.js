d996c9caba848fd9c745ec3aac3a36bfb532b6dd
aratno <aratno@gmail.com>
2015-11-08 14:16:44 -0500

    Going for a bike ride

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-// We'll want to get input for the following later, but for now it's a constant
+// TODO We'll want to get input for the following later, but for now it's a constant
 
-const tier = 10
+const tier = 12
 const omega = 'a'
-const p = ['\\sqrt{a}', // [a] not yet supported by KaTeX
-         '(-a)',
+var p = ['\\sqrt{a}', // [a] not yet supported by KaTeX
+         // '-a', // This one sucks
          '\\frAc{a}{a}', // This is a problem because it replaces 'a' in frac
          'a + a',
-         '(a) - (a)',
+         'a - a',
          'b^{c}',
          '\\sin (a)',
          '\\ln (a)',
@@ -14,9 +14,16 @@ const p = ['\\sqrt{a}', // [a] not yet supported by KaTeX
          // '\\Gamma(a)', This is a problem because it replaces 'a' in Gamma
          'b^e',
          'e^{b}',
-         '(a) - b',
+         'a - b',
          'a + b',
         ]
+
+// Replace all the parentheses with scalable parentheses
+for (var i = 0; i < p.length; i++) {
+  p[i] = p[i].replace('(', '\\left(')
+  p[i] = p[i].replace(')', '\\right)')
+}
+
 // To fix the problem of replacing 'a' in function names, I need to create a
 // shorthand for function names without the letter 'a' in them. For example,
 // replace 'frac' with 'frc'
